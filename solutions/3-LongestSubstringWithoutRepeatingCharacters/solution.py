0be7919932b1cf36d7da5f2d50dbfc5b86cd0b59
Chong Guan <gc771427477@gmail.com>
2018-01-19 08:36:45 +0800

    Add solution for the third problem

---
+++ solutions/3-LongestSubstringWithoutRepeatingCharacters/solution.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+
+    """Docstring for Solution. """
+
+    def __init__(self):
+        """init """
+        pass
+
+    def lengthOfLongestSubstring(self, s):
+        """
+
+        :type s: str
+        :rtype: int
+
+        """
+        longestLen = 0
+        for i, v1 in enumerate(s):
+            subStrList = [v1]
+            curLen = 1
+            for v2 in s[i+1:]:
+                if v2 in subStrList:
+                    if longestLen < curLen:
+                        longestLen = curLen
+                    break
+                else:
+                    subStrList.append(v2)
+                    curLen += 1
+        return longestLen
---
+++ solutions/3-LongestSubstringWithoutRepeatingCharacters/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from solution import Solution
+
+assert Solution().lengthOfLongestSubstring("abcabcbb") == 3
+assert Solution().lengthOfLongestSubstring("bbbbb") == 1
+assert Solution().lengthOfLongestSubstring("pwwkew") == 3
+print "Pass test"
