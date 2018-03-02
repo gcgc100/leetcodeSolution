223923f0ec6beb168a1f959ae1ba6da3435c5193
Chong Guan <gc771427477@gmail.com>
2018-03-02 15:54:32 +0800

    Automatic edit README.md in initSolution.sh

--- initSolution.sh
+++ initSolution.sh
@@ -11,9 +11,13 @@ fi
 
 if [ $# == 3 ]; then
     NAME=$1-$(echo $2 | sed 's/.*problems\/\([^\/]*\)\/description.*/\1/')
-    cp -R template solutions/$NAME || error_exit "copy template failed"
-    cd $3 || error_exit "difficulty dir not found"
+    #cp -R template solutions/$NAME || error_exit "copy template failed"
+    #cd $3 || error_exit "difficulty dir not found"
     
-    ln -s ../solutions/$NAME .
-    cd ..
+    #ln -s ../solutions/$NAME .
+    #cd ..
+    ESCAPEDURL=$(echo $2 | sed 's/\//\\\//g')
+    SEDOPTION="/^TO BE DONE$/s/TO BE DONE/$1. [$NAME]($ESCAPEDURL)\nTO BE DONE/"
+    sed "$SEDOPTION" README.md > tmpREADME
+    mv -f tmpREADME README.md
 fi
